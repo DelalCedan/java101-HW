09ae912cba1b53c99776db27363937512da92df3
DelalCedan <delal.cedan.1994@gmail.com>
2021-07-03 19:34:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        double math, fizik , ph , kimya , turkce;
+        Scanner Number = new Scanner(System.in);
+
+        System.out.print("öğrenciyin adını yazınız :");
+        String name = Number.nextLine();
+
+        System.out.print("math notu = ");
+        math = Number.nextDouble();
+
+        System.out.print("fizik notu = ");
+        fizik = Number.nextInt();
+
+        System.out.print("ph notu = ");
+        ph = Number.nextInt();
+
+        System.out.print("kimya notu = ");
+        kimya = Number.nextInt();
+
+        System.out.print("turkce notu = ");
+        turkce = Number.nextInt();
+
+        double sum = math + fizik + ph + kimya + turkce;
+        double averge = sum/5;
+        System.out.println(name + " ortalama notu : " +averge );
+
+        boolean kosul = averge>=60 ;
+
+        String str = kosul ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(str);
+
+    }
+}
